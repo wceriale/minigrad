6f760b1677d0f11b9396d623b1015c09642a7f37
William Ceriale <w.ceriale11@gmail.com>
2026-02-02 12:38:46 -0500

    add ReLu function

--- minigrad/engine.py
+++ minigrad/engine.py
@@ -59,6 +59,15 @@ class Value:
         out._backward = _backward
         return out
     
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        
+        out._backward = _backward
+        return out
+    
     def exp(self):
         t = math.exp(self)
         out = Value(t, (self, ), 'exp')

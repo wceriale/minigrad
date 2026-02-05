4d7a874a8dc044b95b70d8684fda431936ec7f1d
William Ceriale <w.ceriale11@gmail.com>
2026-02-04 23:06:50 -0500

    get MLP going.

--- makemore/bigramMLP.py
+++ makemore/bigramMLP.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 class BigramMLP:
     # generate an NxN matrix with counts of the occurences
@@ -33,29 +34,15 @@ class BigramMLP:
 
 
         # We are going to map each letter into 2D space
-        self._C = torch.randn(27, 2, generator=g)
-        print('xs size=' + str(self._xs.shape[0]))
-        print('C size=' + str(self._C.shape))
+        self._C = torch.randn(27, 12, generator=g, requires_grad=True)
     
         # emb is n rows -> x context chars -> 2D space representation of char
         self._emb = self._C[self._xs]
 
-        self._W1 = torch.randn(6, 100, generator=g, requires_grad=True)
-        self._b1 = torch.randn(100, generator=g, requires_grad=True)
-        self._W2 = torch.randn(100, 27, generator=g, requires_grad=True)
+        self._W1 = torch.randn(36, 300, generator=g, requires_grad=True)
+        self._b1 = torch.randn(300, generator=g, requires_grad=True)
+        self._W2 = torch.randn(300, 27, generator=g, requires_grad=True)
         self._b2 = torch.randn(27, generator=g, requires_grad=True)
-
-        # h is our hidden layer ;)
-        # h = torch.tanh(self._emb.view(self._emb.shape[0], 6) @ self._W1 + self._b1)
-        # logits = h @ self._W2 + self._b2
-
-        # params = [self._C, self._W1, self._b1, self._W2, self._b2]
-
-        # prob = counts / counts.sum(1, keepdims=True)
-        # row = prob[torch.arange(ys.shape[0]), ys]
-        # loss = -(row.log().mean())
-        # loss = F.cross_entropy(logits, ys)
-        # print(loss)
 
     def params(self):
         return [self._C, self._W1, self._b1, self._W2, self._b2]
@@ -67,32 +54,63 @@ class BigramMLP:
         return self._alphabet[i]
     
 
-    def SGD(self, update_amount, count):
-        # x_enc = F.one_hot(torch.tensor(self._xs), num_classes=27).float()
-
+    def SGD_mini(self, update_amount, count):
+        # lre = torch.linspace(-3, 0, count)
+        # lrs = 10 ** lre
+        # lri = []
+        # lossi = []
         for i in range(count):
-            h = torch.tanh(self._emb.view(self._emb.shape[0], 6) @ self._W1 + self._b1)
-            logits = h @ self._W2 + self._b2
+
+            # Create a mini batch of only 64 elements to check against.
+            ix = torch.randint(0, self._xs.shape[0], (128, )) 
+
+            mini_emb = self._C[self._xs[ix]] # (128, 3, 12)
+
+            h = torch.tanh(mini_emb.view(mini_emb.shape[0], 36) @ self._W1 + self._b1) # (128, 300)
+            logits = h @ self._W2 + self._b2 # (128, 27)
 
             # get loss
-            loss = F.cross_entropy(logits, self._ys)
+            loss = F.cross_entropy(logits, self._ys[ix])
 
             # print(f'loss at step i:{i} = {loss.item()}')
             self._W1.grad = None
             self._W2.grad = None
             self._b1.grad = None
             self._b2.grad = None
+            self._C.grad = None
 
             # Perform back prop
-            loss.backward()      
+            loss.backward()   
+
+            if i > 20000:
+                update_amount = 0.01   
 
             # Update
+            # lr = lrs[i]
             self._W1.data += -update_amount * self._W1.grad
             self._W2.data += -update_amount * self._W2.grad
             self._b1.data += -update_amount * self._b1.grad
             self._b2.data += -update_amount * self._b2.grad
+            self._C.data += -update_amount * self._C.grad
 
-        print('loss at the very end=' + str(loss.item()))
+            # track stats
+            # lri.append(lre[i])
+            # lossi.append(loss.item())
+
+        print('mini loss at the very end=' + str(loss.item()))
+        # plt.plot(lri, lossi)
+        # plt.show()
+
+    def print_loss(self):
+
+        mini_emb = self._C[self._xs] # (total_p, 3, 10)
+
+        h = torch.tanh(mini_emb.view(mini_emb.shape[0], 36) @ self._W1 + self._b1) # (64, 100)
+        logits = h @ self._W2 + self._b2 # (64, 27)
+        # get loss
+        loss = F.cross_entropy(logits, self._ys)
+
+        print('total loss=' + str(loss.item()))
 
     # Create n names
     def makeNames(self, n):
@@ -107,7 +125,7 @@ class BigramMLP:
                 emb = self._C[torch.tensor([curr_context])] # 1, context_size, 2d
 
                 # run it through the NN
-                h = torch.tanh(emb.view(emb.shape[0], 6) @ self._W1 + self._b1)
+                h = torch.tanh(emb.view(emb.shape[0], 36) @ self._W1 + self._b1)
                 logits = h @ self._W2 + self._b2
                 counts = logits.exp()
                 probs = counts / counts.sum(1, keepdim=True)
@@ -130,5 +148,7 @@ b = BigramMLP('names.txt', 3, 0)
 
 print(f"number of parameters={sum(p.nelement() for p in b.params())}")
 # b = BigramMLP('names.txt', 5, 1)
-b.SGD(1, 100)
+# b.SGD(1, 100)
+b.SGD_mini(0.1, 50000)
+b.print_loss()
 print('\n'.join(b.makeNames(10)))
